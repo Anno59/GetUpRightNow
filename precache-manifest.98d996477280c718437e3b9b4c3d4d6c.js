6d4c04af3dc7ac4306e87b912cf9855d5eaaea78
Anno59 <372025284@qq.com>
2019-06-22 19:36:02 +0800

    Updates

---
+++ precache-manifest.98d996477280c718437e3b9b4c3d4d6c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40755452111b5afa5acaacc047b9a5e5",
+    "url": "/getuprightnow/index.html"
+  },
+  {
+    "revision": "673c782a6ed3039af29f",
+    "url": "/getuprightnow/static/js/2.c82ea2c6.chunk.js"
+  },
+  {
+    "revision": "59f5dfd446cafa474a44",
+    "url": "/getuprightnow/static/js/main.79bc4f13.chunk.js"
+  },
+  {
+    "revision": "18f08b3bc7bd07c5fddb",
+    "url": "/getuprightnow/static/js/runtime~main.b215c864.js"
+  }
+]);
